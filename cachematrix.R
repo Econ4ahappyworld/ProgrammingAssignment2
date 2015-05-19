84cdef545fae94e27b6a08e80b275802af9447a8
econ4ahappyworld <timothypaulwilson@gmail.com>
2015-05-19 16:32:22 +0200

    Fixed typo in code - this version works for submission. FINAL SUBMISSION

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,16 @@
 ##FUNCTION 1
 ## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) {
         m <- NULL
-        set <- function(y){
+        set <- function(y) {
                 x <<- y
-                m <<- NULL                
+                m <<- NULL
         }
         get <- function() x
         setsolve <- function(solve) m <<- solve
         getsolve <- function() m
-        list(set = set, get = get, 
+        list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
@@ -26,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## from the cache.
 
 cacheSolve <- function(x, ...) {
-        m <- x$getsolve
+        m <- x$getsolve()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)                
@@ -36,3 +36,18 @@ cacheSolve <- function(x, ...) {
         x$setsolve(m)
         m
 }
+
+## TEST
+## 
+
+vector <- makeCacheMatrix(matrix(c(9,10,3,20,25,30,35,40,45,50,3,8,65,70,75,80), 
+                            nrow=4, byrow=T))
+vector
+
+cacheSolve(vector)
+
+## The above result should be the invesrse of the initial vector
+
+cacheSolve(vector)
+## The above result should be drawn from the cache and be the inverse of the 
+## initial vector
